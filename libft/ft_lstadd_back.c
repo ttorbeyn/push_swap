6534acad8a3c1e2ce720bc3907162da6431de933
ttorbeynH <ttorbeyn@student.s19.be>
2022-01-02 14:53:54 +0100

    save

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ttorbeyn <ttorbeyn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 23:47:16 by ttorbeyn          #+#    #+#             */
-/*   Updated: 2021/01/13 00:16:46 by hubert           ###   ########.fr       */
+/*   Updated: 2021/12/30 21:23:26 by hubert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 {
 	t_list	*current;
 
-	if (alst == 0 || new == 0)
+	if (!alst || !new)
 		return ;
 	current = *alst;
 	if (current == 0)
